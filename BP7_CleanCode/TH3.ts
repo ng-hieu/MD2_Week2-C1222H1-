fd82daf7a6fcae3a04b4f028256ae629e21ccda8
Your Name <you@example.com>
2023-02-17 13:31:50 +0700

    Tach phuong thuc

---
+++ BP7_CleanCode/TH3.ts
@@ -0,0 +1,16 @@
+class Cylinder {
+    private static getBaseArea(radius:number):number{
+        return Math.PI * radius * radius;
+}
+    private static getPerimeter(radius:number):number{
+        return 2 * Math.PI * radius;
+    }
+     public static getVolume(radius: number, height: number): number {
+        let baseArea: number = this.getBaseArea(radius);
+        let perimeter: number = this.getPerimeter(radius);
+        let volume: number = perimeter * height + 2 * baseArea;
+        return volume;
+    }
+}
+let result = Cylinder.getVolume(5, 10);
+console.log(result);
